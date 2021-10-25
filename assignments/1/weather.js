9f24df271d6ea0dc4f81023b5a16a9d578544fd6
Kilbane <kilbanen@tcd.ie>
2021-10-25 14:34:03 +0100

    Made the client fetch data from the server

--- assignments/1/weather.js
+++ assignments/1/weather.js
@@ -1,15 +1,16 @@
 const apiKey = process.env.API_KEY
 const express = require('express')
 const axios = require('axios')
+const cors = require('cors')
 const app = express()
 const port = 3000
-
-app.get('/', getWeatherData)
+app.use(cors())
+app.get('/weather', getWeatherData)
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 function getWeatherData(req, res) {
   axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Dublin,Ireland&APPID=${apiKey}`)
-       .then(response => response.data)
-       .then(response => console.log(response))
+       .then(response => res.json(response.data))
        .catch(err => console.log(err))
 }
 
