89e437befe81fb5c1bdccea0da3fe67f63ce1209
Kilbane <kilbanen@tcd.ie>
2021-10-27 13:25:36 +0100

    Added pollution API request to server

--- assignments/1/weather.js
+++ assignments/1/weather.js
@@ -6,11 +6,20 @@ const app = express()
 const port = 3000
 app.use(cors())
 app.get('/weather/:city', getWeatherData)
+app.get('/pollution/:lat/:lon', getPollutionData)
 
 function getWeatherData(req, res) {
   let city = req.params.city
   axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${apiKey}`)
        .then(response => res.json(response.data))
+       .catch(err => console.log(err))
+}
+
+function getPollutionData(req, res) {
+  let lat = req.params.lat
+  let lon = req.params.lon
+  axios.get(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+       .then(response => console.log(response.data))
        .catch(err => console.log(err))
 }
 
