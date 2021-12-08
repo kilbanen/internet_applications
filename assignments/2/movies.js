2ba2d3c9d867e129e1a96324a451c302f3fd735e
Kilbane <kilbanen@tcd.ie>
2021-12-08 19:34:27 +0000

    Implemented destroying database

--- assignments/2/movies.js
+++ assignments/2/movies.js
@@ -95,8 +95,23 @@ const uploadMovies = () => {
   });
 };
 
+const destroyTable = () => {
+  var params = {
+    TableName : "Movies"
+  };
+
+  dynamodb.deleteTable(params, function(err, data) {
+      if (err) {
+          console.error("Unable to delete table. Error JSON:", JSON.stringify(err, null, 2));
+      } else {
+          console.log("Deleted table. Table description JSON:", JSON.stringify(data, null, 2));
+      }
+  });
+}
+
 app.use(cors())
 app.get('/create', createNewTable)
+app.get('/destroy', destroyTable)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
